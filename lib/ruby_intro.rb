d66eae66e9c1c5f943165f185f4d2078a427a9df
Edwin Bratini <edwin.bratini@gmail.com>
2015-11-03 01:16:35 +0000

    Finish hw0 - ruby intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.max(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  arr.permutation(2).each do |p|
+    return true if p.reduce(:+) == n
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{ name }"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[a-z&&[^aeiou]]/i ? true : false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+  s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError, 'isbn cannot be empty' if isbn.empty?
+    raise ArgumentError, 'price cannot be <= 0' if price <= 0
+    @isbn  = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    '%s%.2f' % ['$', self.price]
+  end
 end
